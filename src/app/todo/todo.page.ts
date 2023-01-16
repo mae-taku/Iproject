c7ccd9424b9309f6846ea73446c3a93874255571
maetaku <>
2023-01-16 21:57:49 +0900

    Todo APP作成開始

---
+++ src/app/todo/todo.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.page.html',
+  styleUrls: ['./todo.page.scss'],
+})
+export class TodoPage implements OnInit {
+  title = 'タスク登録';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
