7c674e38ff9d2b4d1e8fe88941ac6f919f52877a
maetaku <>
2023-01-18 15:43:52 +0900

    アクションシート実装

--- src/app/task-list/task-list.page.ts
+++ src/app/task-list/task-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
+import { actionSheetController } from '@ionic/core';
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.page.html',
@@ -7,18 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskListPage implements OnInit {
   tasks: { name: string }[] = [
-    { name: 'タスク1'},
-    { name: 'タスク2'},
+    { name: 'タスク1' },
+    { name: 'タスク2' },
   ];
 
-  constructor() { }
+  constructor(
+    public actionSheetController: ActionSheetController,
+  ) { }
 
   ngOnInit() {
   }
-  ionVieWillEnter(){
-    if('tasks' in localStorage){
-      this.tasks = JSON.parse(localStorage.getItem("tasks")||"");
+  ionVieWillEnter() {
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage.getItem("tasks") || "");
     }
   }
+
+  /**
+   * async/await 非同期処理
+   * 他の処理を待たずに他の処理を行う。
+   */
+  async doChangeTask() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Change',
+        icon: 'create',
+        handler: () => {
+          console.log('Archive clicked');
+        }
+      }, {
+        text: 'Close',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+
+    await actionSheet.present();
+  }
+
 
 }
