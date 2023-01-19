17c50b23420aa4596033d68af18a4f3b7d6768d0
maetaku <>
2023-01-20 08:25:00 +0900

    記事のプロパティを設定。

--- src/app/out-resource/out-resource.page.ts
+++ src/app/out-resource/out-resource.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutResourcePage implements OnInit {
 
-  constructor() { }
+  // 記事のプロパティ
+  posts: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string
+  }[] = [];
+
+  constructor(
+    ) {}
 
   ngOnInit() {
   }
